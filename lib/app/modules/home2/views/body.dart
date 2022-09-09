093d2a71da8908a67102c826482bd9f9dd3354b5
ummatisoftwares <ummatisoftwares@gmail.com>
2022-09-09 22:00:06 +0300

    init

--- lib/app/modules/home2/views/body.dart
+++ lib/app/modules/home2/views/body.dart
@@ -12,6 +12,57 @@ Center body(BuildContext context) {
       child: Wrap(
         alignment: WrapAlignment.center,
         children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+            width: 400,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      maxRadius: 36,
+                      child: Image.asset("assets/helper.png"),
+                    ),
+                    const SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 240,
+                          height: 36,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+                            border: Border.all(width: 1.0, color: Colors.black26,),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: const [
+                                Text("Add Profile Photo", style: TextStyle(fontSize: 16),),
+                                Icon(Icons.add_circle_outline),
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Container(
+                            padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                            child: const Text("Add a profile to make it more personal.\nIt makes a difference", style: TextStyle(color: Colors.black54, fontSize: 12),)
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 12,),
+                Divider(color: Colors.black87, thickness: 0.8,),
+                SizedBox(height: 6,),
+                Divider(color: Colors.black87, thickness: 0.6,)
+              ],
+            ),
+          ),
           normalTextField(context, "Full Name", "Your Full name"),
           selectTextField(context, "Select Your Gender"),
           dateTextField(context, "Choose Your Date of Birth"),
